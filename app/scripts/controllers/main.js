9b8cd969e9648a3e5aa088e078ed21ba8f24f997
vogtn <nicolasjvogt@gmail.com>
2017-01-24 16:49:06 -0800

    adding basic functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the yeoAppApp
  */
 angular.module('yeoAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'Mangos'
     ];
   });
